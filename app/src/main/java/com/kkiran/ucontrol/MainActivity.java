05844f57b0e7ee13be1af70e5c2dc6306beb7324
Kiran Kanchamreddy <kiran.kanchamreddy@gmail.com>
2016-05-24 22:09:25 -0700

    Update MainActivity.java

--- app/src/main/java/com/kkiran/ucontrol/MainActivity.java
+++ app/src/main/java/com/kkiran/ucontrol/MainActivity.java
@@ -11,4 +11,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     //TODO:
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
